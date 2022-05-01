209e90e47cca49167b8ab0765e26658973557771
kipplesunderscore <kipples+github@kipples.net>
2022-05-02 03:53:40 +0930

    fix exponential extra counting of transactions

--- upbank_csv/upbank_csv.py
+++ upbank_csv/upbank_csv.py
@@ -1,15 +1,17 @@
 import requests
+import time
 from datetime import datetime, timezone
 import calendar
 from pprint import pprint
 import csv
 import argparse
+from decimal import *
 
 class Upbank:
 
     base_url = 'https://api.up.com.au/api/v1'
 
-    page_size = 80
+    page_size = 10
     
     def __init__(self, access_token):
         self._headers = {'Authorization': 'Bearer ' + access_token}
@@ -25,9 +27,20 @@ class Upbank:
                 transaction['transaction_type'] = "Purchase"
             else:
                 transaction['transaction_type'] = "Payment Received"
-
         return transaction
-        
+
+    def calculate_category(self, transaction):
+        if transaction['relationships']['category']['data']:
+            response = self.request(transaction['relationships']['category']['links']['related'])
+            transaction['category'] = response['data']['attributes']['name']
+        else:
+            transaction['category'] = ''
+        return transaction
+
+    def calculate_relationships(self, transaction):
+        transaction = self.calculate_transaction_type(transaction)
+        transaction = self.calculate_category(transaction)
+        return transaction
 
     def transactions(self, year, month):
         month_begin = datetime(year, month, 1).astimezone().isoformat()
@@ -38,17 +51,32 @@ class Upbank:
 
         response = self.request(self.base_url + '/transactions', params)
 
-        response['data'] = map(self.calculate_transaction_type, response['data'])
+        transactions = []
+        for t in response['data']:
+            transactions.append(self.calculate_relationships(t))
 
-        yield(response['data'])
+        yield(transactions)
 
         while(response['links']['next']):
             response = self.request(response['links']['next'], params)
-            response['data'] = map(self.calculate_transaction_type, response['data'])
-            yield(response['data'])
+
+            transactions = []
+            for t in response['data']:
+                transactions.append(self.calculate_relationships(t))
+
+            yield(transactions)
 
     def request(self, endpoint, params = {}):
-        response = requests.get(endpoint, params = params, headers = self._headers)
+        max_attempts = 10
+        attempt = 0
+        while attempt < max_attempts:
+            response = requests.get(endpoint, params = params, headers = self._headers)
+            if response.status_code != 429:
+                break
+
+            time.sleep(2**attempt)
+            attempt = attempt + 1
+            
 
         return response.json()
 
@@ -56,7 +84,7 @@ class Upbank:
 def output_csv_for_month(year, month, access_token_file):
     with open(access_token_file, 'r') as file:
         with open(f'./{year}-{month:02}.csv', 'w') as csvfile:
-            fieldnames = ['id', 'transaction_type', 'description', 'raw_text', 'message', 'amount', 'roundup', 'created', 'settled']
+            fieldnames = ['id', 'transaction_type', 'category', 'description', 'raw_text', 'message', 'amount', 'roundup', 'subtotal', 'created', 'settled']
             access_token = file.read().rstrip()
             
             upbank = Upbank(access_token)
@@ -66,20 +94,28 @@ def output_csv_for_month(year, month, access_token_file):
             csv_writer.writeheader()
             for page in upbank.transactions(year, month):
                 for transaction in page:
+                    description = transaction['attributes']['description']
+                    if 'Round Up' in description:
+                        continue
+                    amount = transaction['attributes']['amount']['value']
+                    roundup = transaction['attributes']['roundUp']['amount']['value'] if transaction['attributes']['roundUp'] else '0'
+                    subtotal = str(Decimal(amount) + Decimal(roundup))
                     transactions.append(
                         {
                             'id': transaction['id'],
                             'transaction_type': transaction['transaction_type'],
-                            'description': transaction['attributes']['description'],
+                            'category': transaction['category'],
+                            'description': description,
                             'raw_text': transaction['attributes']['rawText'],
-                            'amount': transaction['attributes']['amount']['value'],
-                            'roundup': transaction['attributes']['roundUp']['amount']['value'] if transaction['attributes']['roundUp'] else '0',
+                            'amount': amount,
+                            'roundup': roundup,
+                            'subtotal': subtotal,
                             'created': datetime.fromisoformat(transaction['attributes']['createdAt']).strftime("%Y/%m/%d"),
                             'settled': datetime.fromisoformat(transaction['attributes']['settledAt']).strftime("%Y/%m/%d"),
                         }
                     )
-                    
-                    csv_writer.writerows(transactions)
+
+            csv_writer.writerows(transactions)
 
 def main():
     parser = argparse.ArgumentParser(description="Up Bank CSV Exporter.")
@@ -90,3 +126,5 @@ def main():
     args = parser.parse_args()
 
     output_csv_for_month(args.year, args.month, args.token_file)
+
+output_csv_for_month(2021, 10, './access_token')
